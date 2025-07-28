bb99e6dc6a29c2fc04a4facd148a289ebd46cd0a
Sahithi Korlapati <sahithi2908@gmail.com>
2025-07-28 11:16:01 -0700

    Modified backend scripts

--- backend/scripts/setup_mysql.sql
+++ backend/scripts/setup_mysql.sql
@@ -2,6 +2,6 @@
 CREATE DATABASE IF NOT EXISTS pmb_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- Create the user and grant privileges
-CREATE USER IF NOT EXISTS 'pmb_user'@'localhost' IDENTIFIED BY 'pmb_user';
-GRANT ALL PRIVILEGES ON pmb_db.* TO 'pmb_user'@'localhost';
+CREATE USER IF NOT EXISTS 'pmb_user'@'%' IDENTIFIED BY 'pmb_user';
+GRANT ALL PRIVILEGES ON pmb_db.* TO 'pmb_user'@'%';
 FLUSH PRIVILEGES;
