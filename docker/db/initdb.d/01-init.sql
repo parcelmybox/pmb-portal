d2298078574d6f3a926beda2a654bc13b4666905
Sahithi Korlapati <sahithi2908@gmail.com>
2025-07-28 11:43:51 -0700

    Remove 000x migration files

---
+++ docker/db/initdb.d/01-init.sql
@@ -0,0 +1,7 @@
+-- Create the database
+CREATE DATABASE IF NOT EXISTS pmb_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Create the user and grant privileges
+CREATE USER IF NOT EXISTS 'pmb_user'@'%' IDENTIFIED BY 'pmb_user';
+GRANT ALL PRIVILEGES ON pmb_db.* TO 'pmb_user'@'%';
+FLUSH PRIVILEGES;
